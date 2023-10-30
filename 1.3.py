b26b55ecf841dd26c55007f3e09fd576d704a410
tranvantiep2501 <148097757+tranvantiep2501@users.noreply.github.com>
2023-10-31 00:12:07 +0700

    Add files via upload

---
+++ 1.3.py
@@ -0,0 +1,13 @@
+#đơn giá sản phẩm là 25000
+#số lượng sản phẩm là
+
+
+dongia = float(input("đơn giá của sản phẩm:"))
+soluong = float(input("số lượng của sản phẩm là:"))
+thanhtien = soluong * dongia
+print('Tên Hàng: Sửa Hộp ')
+print('Số Lượng', soluong)
+print('Đơn Giá', dongia)
+print('Thành Tiền', thanhtien)
+
+
